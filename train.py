d701c0f94e075eb0cdb57de8a124da8d835f95bc
maryam-fatima <62232940+maryam-fatima@users.noreply.github.com>
2021-09-02 11:54:20 +0500

    Update train.py

--- train.py
+++ train.py
@@ -3,5 +3,5 @@
 # for 20 music (10 classical/10 metal) song samples
 from pyAudioAnalysis.audioTrainTest import extract_features_and_train
 mt, st = 1.0, 0.05
-dirs = ["data/music/classical", "data/music/metal"] 
-extract_features_and_train(dirs, mt, mt, st, st, "svm_rbf", "svm_classical_metal")
+dirs = ["data/chainsaw", "data/axe", "data/random"] 
+extract_features_and_train(dirs, mt, mt, st, st, "svm_rbf", "svm_deforest")
