256a868d0748e9cfd57a55aad2f6821e9e791b35
maryam-fatima <62232940+maryam-fatima@users.noreply.github.com>
2021-09-07 11:24:19 +0500

    Update train.py

--- train.py
+++ train.py
@@ -7,5 +7,5 @@ os.chdir("/content/gdrive/MyDrive/project_folder/Wood cutting sound")
 
 mt, st = 1.0, 0.05
 #dirs = ["data/chainsaw", "data/axe", "data/random"]
-dirs = ["Training_data\ambiguous sound", "Training_data\axe cutting sound", "Training_data\back_ground noise", "Training_data\chainsaw cutting sound", "Training_data\manual saw cutting sound", "Training_data\tree breaking sound"] 
+dirs = ["Training_data/ambiguous sound", "Training_data/axe cutting sound", "Training_data/back_ground noise", "Training_data/chainsaw cutting sound", "Training_data/manual saw cutting sound", "Training_data/tree breaking sound"] 
 extract_features_and_train(dirs, mt, mt, st, st, "svm", "svm_deforest")
