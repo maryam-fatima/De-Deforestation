e545523045d21d965fea057dc3843df1e3c9ae1f
maryam-fatima <62232940+maryam-fatima@users.noreply.github.com>
2021-09-07 10:50:51 +0500

    Update train.py

--- train.py
+++ train.py
@@ -3,5 +3,6 @@
 # for 20 music (10 classical/10 metal) song samples
 from pyAudioAnalysis.audioTrainTest import extract_features_and_train
 mt, st = 1.0, 0.05
-dirs = ["data/chainsaw", "data/axe", "data/random"] 
+#dirs = ["data/chainsaw", "data/axe", "data/random"]
+dirs = ["Training_data\ambiguous sound", "Training_data\axe cutting sound", "Training_data\back_ground noise", "Training_data\chainsaw cutting sound", "Training_data\manual saw cutting sound", "Training_data\tree breaking sound"] 
 extract_features_and_train(dirs, mt, mt, st, st, "svm", "svm_deforest")
