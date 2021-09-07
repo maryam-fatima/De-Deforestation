88fc3b9b4163763c1fa896299c3e43e54ac84f7a
maryam-fatima <62232940+maryam-fatima@users.noreply.github.com>
2021-09-07 10:54:07 +0500

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,8 @@
 # to extract feature and train SVM classifier 
 # for 20 music (10 classical/10 metal) song samples
 from pyAudioAnalysis.audioTrainTest import extract_features_and_train
+chdir("/content/gdrive/MyDrive/project_folder/Wood cutting sound")
+
 mt, st = 1.0, 0.05
 #dirs = ["data/chainsaw", "data/axe", "data/random"]
 dirs = ["Training_data\ambiguous sound", "Training_data\axe cutting sound", "Training_data\back_ground noise", "Training_data\chainsaw cutting sound", "Training_data\manual saw cutting sound", "Training_data\tree breaking sound"] 
