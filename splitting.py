18c0e2cc2a22e4b513b03da11870cbb0276dc258
Zainab Ghauri <66675256+ZainabGhauri@users.noreply.github.com>
2021-09-12 12:36:06 +0500

    Delete splitting.py

--- splitting.py
+++
@@ -1,8 +0,0 @@
-import os
-os.chdir("/content/gdrive/MyDrive/project_folder/")
-dirs = ["Cutting/Cutting", "NonCutting/NonCutting"]
-
-from sklearn.datasets import load_iris
-#Iris is available from the sklearn package
-iris = load_iris(dirs)
-X, y = iris.data, iris.target
